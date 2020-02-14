209f5d39593ffa1daebbe267d0641544b42c6bc1
nfaith <49287441+nfaith@users.noreply.github.com>
2020-02-14 09:10:00 -0700

    Add files via upload
    This is not the completed version yet.

---
+++ lab3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+
+/* Faith Nelson
+ * Lab 3, CSCI 112
+ * 2/12/2020
+*/
+
+int findNumbers(int one, int two, int three){
+	int biggest, smallest, average;
+
+}	
+
+int randomNums(int amount, int start, int end){
+
+}	
+
+
+int main(void){
+
+	return(0);	
+
+}	
