3637d11293958e741fa25b794a0aa69acb7024b2
nfaith <49287441+nfaith@users.noreply.github.com>
2020-03-06 11:57:09 -0700

    Add files via upload
    Not complete

---
+++ lab5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/* Faith Nelson
+ * Lab 5, CSCI 112
+ * March 8, 2020
+*/ 
+
+
+
+
+int main(void){
+	char COLOR_CODES[10][7] = {"black","brown","red","orange","yellow","green","blue","violet","grey", "white"}
+	return (0);
+}	
